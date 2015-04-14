8486cbfdd64c80d80bf39e5b52546e964a59394e
IKEDA YOSUKE <ikeda@ale-cole.com>
2015-04-14 13:52:04 +0900

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: 'テスト商品1', price: 1500)
+Product.create(name: 'テスト商品2', price: 2500)
+Product.create(name: 'テスト商品3', price: 4000)
+Product.create(name: 'テスト商品4', price: 500)
+Product.create(name: 'テスト商品5', price: 10000)
